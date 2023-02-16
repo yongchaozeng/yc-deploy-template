2481c4db560212df136e9538e661c9226d094209
zhonghe <zhonghe@bicitech.cn>
2023-02-16 10:10:27 +0800

    feat: 测试下载命令

--- deploy.config.js
+++ deploy.config.js
@@ -5,7 +5,7 @@ module.exports = {
   // 根据需要进行配置，如只需部署prod线上环境，请删除dev测试环境配置，反之亦然，支持多环境部署
   dev: {
     // 测试环境
-    name: "测试环境",
+    name: "测试环境1",
     script: "npm run build", // 测试环境打包脚本
     host: "", // 测试服务器地址
     port: 22, // ssh port，一般默认22
